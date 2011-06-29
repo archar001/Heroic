fce217b422c9b1f47198a723f4249b3ad4bfd28c
sami <abdulsami.isb@gmail.com>
2011-06-29 16:54:43 +0200

    gold script

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -596,7 +596,8 @@ void AddSC_wintergrasp();
 void AddSC_chat_log();
 
 // custom
-
+ void AddSC_custom_levelgold();
+ void AddSC_duelreset();
 #endif
 
 void AddScripts()
@@ -1233,6 +1234,7 @@ void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
-
+  AddSC_custom_levelgold();
+  AddSC_duelreset();
 #endif
 }
